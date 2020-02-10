9fd5868f6386c8d6f114a4ada9a1c1d32d18e2a4
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-02-10 15:17:19 +0300

    Update task1.cpp

--- Lab_1/task1.cpp
+++ Lab_1/task1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	float gallons, cufeet;
+
+	cout << "Введите чилсо Галлонов : ";
+	cin >> gallons;
+
+	cufeet = gallons / 7.481;
+
+	cout << "Эквивалентный объем в кубических футах : " << cufeet << endl;
+
+	system("pause");
+	return 0;
+}
