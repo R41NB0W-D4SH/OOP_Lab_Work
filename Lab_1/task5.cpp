51b7cf8a273779fd858165f1008783c9654c18ef
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-02-10 15:47:26 +0300

    Update task5.cpp

--- Lab_1/task5.cpp
+++ Lab_1/task5.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	char a;
+	cout << "Введите символ(букву) : ";
+	cin >> a;
+
+	if (islower(a)) cout << "1" << endl;
+	else cout << "0" << endl;
+
+	system("pause");
+	return 0;
+}
