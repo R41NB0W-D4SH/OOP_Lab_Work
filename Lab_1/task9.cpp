e7698c41d3863a7e76e283df891fa92e92d359f8
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-02-19 20:19:17 +0300

    Update task9.cpp

--- Lab_1/task9.cpp
+++ Lab_1/task9.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+  int a, b, c, d;
+  char e;
+
+  cin >> a >> e >> b;
+  cin >> c >> e >> d;
+
+  cout << a * d + b * c << e << b * d;
+
+	system("pause");
+	return 0;
+}
