249071a4b8a09064c9a36ccd5f539ec8d31ca6a5
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-02-19 20:43:01 +0300

    Update task10.cpp

--- Lab_1/task10.cpp
+++ Lab_1/task10.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    setlocale (0, "");
+    int f1, s, p1;
+    double f2;
+    cout << "Введите фунты : ";
+    cin >> f1;
+    cout << "Введите шиллинги : ";
+    cin >> s;
+    cout << "Введите пенсы : ";
+    cin >> p1;
+
+    f2 = f1 * 240 + s * 12 + p1;
+
+    cout << "Старый формат фунтов стерлингов: " << f1 <<"." << s << "." << p1 << endl;
+    cout << "Новый формат фунтов стерлингов: " << f2 / 240 << endl;
+
+	system("pause");
+	return 0;
+}
