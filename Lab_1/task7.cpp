7635ba40b83579dfc01006a18c3cfc7400a5cc7c
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-02-19 20:06:50 +0300

    Update task7.cpp

--- Lab_1/task7.cpp
+++ Lab_1/task7.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    double c;
+
+    cout << "Введите градусы по Цельсию : ";
+    cin >> c;
+
+    cout << c << " C = " << c * 9 / 5 + 32 << " F" << endl;
+
+    system("pause");
+    return 0;
+}
