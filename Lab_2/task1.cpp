ba8f2b32bd49b8f39b630d3b372fbc1b77959c15
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-02-26 20:42:59 +0300

    Update task1.cpp

--- Lab_2/task1.cpp
+++ Lab_2/task1.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <iomanip>
 
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    unsigned long n;
+
+    cout << "Введите число: ";
+    cin >> n;
+
+    for(int j = 1; j <= 200; j++)
+        {
+        cout << setw(5) << j * n << "  ";
+        if(j % 10 == 0)
+            cout << endl;
+        }
+
+    system("pause");
+    return 0;
+}
