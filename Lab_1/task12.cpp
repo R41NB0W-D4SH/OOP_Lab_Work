6ae46b83a926a4df4592e88ab022e8679abc650a
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-02-26 20:12:46 +0300

    Update task12.cpp

--- Lab_1/task12.cpp
+++ Lab_1/task12.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	float df, all, f, raz1, raz2, s, p;
+
+	cout << "Введите фунты стерлингов в современном формате: ";
+	cin >> df;
+
+    all = df * 240;
+	df = static_cast<int>(df);
+    f = df * 240;
+    raz1 = all - f;
+    raz1 = static_cast<int>(raz1);
+    s = raz1 / 12;
+    s = static_cast<int>(s);
+    raz2 = s * 12;
+    p = raz1 - raz2;
+
+    cout << df << "." << s << "." << p << endl;
+
+	system("pause");
+	return 0;
+}
