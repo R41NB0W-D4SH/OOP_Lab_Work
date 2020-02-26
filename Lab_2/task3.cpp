21367bcb5b019b85301b75b9b16d306ccafeb14d
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-02-26 21:49:10 +0300

    Update task3.cpp

--- Lab_2/task3.cpp
+++ Lab_2/task3.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <conio.h>
 
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    char ch;
+    unsigned long total = 0;
+
+    cout << "Введите число: ";
+
+    while( (ch=getche()) != '\r' )
+        total = total*10 + ch-'0';
+    cout << "\nЧисло: " << total << endl;
+
+    system("pause");
+    return 0;
+}
