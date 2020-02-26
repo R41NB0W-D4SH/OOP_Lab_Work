9d6f1990b13185f7862aab7b7c73d901106db1b2
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-02-26 20:53:22 +0300

    Update task2.cpp

--- Lab_2/task2.cpp
+++ Lab_2/task2.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int response;
+    double temper;
+
+   cout << "1 - для перевода температуры из градусов Фаренгейта в Цельсия,"
+	<< "\n2 - для перевода температуры из градусов Цельсия в Фаренгейт : ";
+
+   cin >> response;
+
+   if( response == 1 )
+      {
+      cout << "Введите температуру в Фаренгейтах : ";
+      cin >> temper;
+      cout << "В градусах Цельсия : " << 5.0/9.0*(temper-32.0);
+      }
+   else
+      {
+      cout << "Введите температуру в Цельсиях : ";
+      cin >> temper;
+      cout << "В градусах Фаренгейта : " << 9.0/5.0*temper + 32.0;
+      }
+
+   cout << endl;
+
+   system("pause");
+   return 0;
+}
