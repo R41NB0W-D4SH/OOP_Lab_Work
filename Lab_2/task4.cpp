c6e59b0e061bdcb3b651da8e8046ed0a4527848c
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-08 18:06:51 +0300

    Update task4.cpp

--- Lab_2/task4.cpp
+++ Lab_2/task4.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    double n1, n2, ans;
+    char oper, ch;
+
+    do
+    {
+        cout << "Введите первое число, потом оператор, затем второй число: ";
+        cin >> n1 >> oper >> n2;
+
+        switch(oper)
+        {
+            case '+':  ans = n1 + n2;  break;
+            case '-':  ans = n1 - n2;  break;
+            case '*':  ans = n1 * n2;  break;
+            case '/':  ans = n1 / n2;  break;
+            default:   ans = 0;
+        }
+        cout << "Ответ = " << ans;
+        cout << "\nПроводить операции еще? \nВведите 'y' для - Да\nВведите 'n' для - Нет\n";
+        cin >> ch;
+    }
+
+    while( ch != 'n' );
+
+    system("pause");
+    return 0;
+}
