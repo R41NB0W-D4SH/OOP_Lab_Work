a6094e2965960c0512b1b9ff5856654249e33362
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-15 17:49:46 +0300

    Update task7.cpp

--- Lab_2/task7.cpp
+++ Lab_2/task7.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    unsigned int time;
+    double per, dep;
+
+    cout << "Введите начальный вклад: ";
+    cin >> dep;
+    cout << "Введите число лет: ";
+    cin >> time;
+    cout << "Введите процентную ставку: ";
+    cin >> per;
+
+    per /= 100;
+
+    for (int i = 0; i < time; i++)
+    {
+        dep = dep + (dep * per);
+    }
+
+    cout << "Через " << time << " лет вы получите: " << dep << " доллара" << endl;
+
+    system("pause");
+    return 0;
+}
