0abd4ad47efcab6244ce1eb201117452b7bcc019
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-15 16:59:29 +0300

    Update task5.cpp

--- Lab_2/task5.cpp
+++ Lab_2/task5.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 20; i++)
+    {
+        cout << string(i, 'X') << endl;
+    }
+
+    system("pause");
+    return 0;
+}
