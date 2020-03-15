647b278d8fd27686322b25ded6172206f7a0539d
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-15 17:35:12 +0300

    Update task6.cpp

--- Lab_2/task6.cpp
+++ Lab_2/task6.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    unsigned int n;
+    unsigned long fact = 1;
+
+   do//Для цикличного спроса чисел, и высчитывания факториалов
+   {
+        cout << "Введите число: ";
+        cin >> n;
+        if (n > 0)
+        {
+            for (int j = n; j > 0; j--)//Высчитывает факториал
+            {
+                fact *= j;
+            }
+            cout << "Факториал : " << fact << endl;
+            fact = 1;
+        }
+        else return 0;//Условие выхода из цикла, если переменная == ноль
+   }
+   while (fact > 0);
+
+   system("pause");
+   return 0;
+}
