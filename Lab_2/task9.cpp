01776b7fe2095d4502d3b9b2581a8302f446ec64
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-16 14:52:19 +0300

    Update task9.cpp

--- Lab_2/task9.cpp
+++ Lab_2/task9.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int stul, gost, var = 1;
+
+	cout << "Введите количество мест : ";
+	cin >> stul;
+	cout << "Введите количество гостей : ";
+	cin >> gost;
+
+	for (int i = 0; i < stul; i++)
+	{
+		var = var * gost;
+		gost--;
+	}
+
+	cout << "Число возможных рассадок : " << var << endl;
+
+	system("pause");
+	return 0;
+}
