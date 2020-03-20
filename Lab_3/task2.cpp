19e3b3ec77a93eea22d79359cfbe4971725d42c1
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-20 20:37:36 +0300

    Update task2.cpp

--- Lab_3/task2.cpp
+++ Lab_3/task2.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+using namespace std;
+
+struct point
+{
+   int x;
+   int y;
+};
+
+int main()
+{
+    setlocale(0, "");
+    point p1, p2, p3;
+
+    cout << "Введите координаты для первой точки : ";
+    cin >> p1.x >> p1.y;
+    cout << "Введите координаты для второй точки : ";
+    cin >> p2.x >> p2.y;
+
+    p3.x = p1.x + p2.x;
+    p3.y = p1.y + p2.y;
+
+    cout << "Сумма координат первой и второй точек : " << p3.x << ", " << p3.y << endl;
+
+    system("pause");
+    return 0;
+}
