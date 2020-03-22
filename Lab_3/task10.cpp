589b6291a2b845687c5a80ee31e543bd94e00dec
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-22 16:23:44 +0300

    Update task10.cpp

--- Lab_3/task10.cpp
+++ Lab_3/task10.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+using namespace std;
+
+struct sterling
+{
+    int f;
+    int sh;
+    int p;
+};
+
+int main()
+{
+    setlocale(0, "");
+    double dec;
+    cout <<"Введите десятичные фунты: "; cin >>dec;
+
+    sterling st1;
+    st1.f = dec;
+    st1.sh = (dec-st1.f) * 20;
+    st1.p = (((dec-st1.f) * 20)-st1.sh) * 12;
+
+    cout <<"Старый вид: " <<st1.f <<"." <<st1.sh <<"." <<st1.p <<endl;
+
+    system ("pause");
+    return 0;
+}
