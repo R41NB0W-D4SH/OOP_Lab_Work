6d2eb00fc0c6b85301095626235ad59f38e779f8
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-22 16:08:30 +0300

    Update task6.cpp

--- Lab_3/task6.cpp
+++ Lab_3/task6.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
 
+using namespace std;
+
+enum etype {laborer, secretary, manager, accountant, executive, researcher};
+
+int main()
+{
+    setlocale (0, "");
+    char ch;
+    etype sm;
+    cout <<"Введите первую букву должности (laborer, secretary, manager, accountant, executive, researcher) : ";
+    cin >>ch;
+    switch(ch)
+    {
+    case 'l': sm = laborer; break;
+    case 's': sm = secretary; break;
+    case 'm': sm = manager; break;
+    case 'a': sm = accountant; break;
+    case 'e': sm = executive; break;
+    case 'r': sm = researcher; break;
+    }
+    cout <<"Полное название должности: ";
+    switch(sm)
+    {
+    case 0: cout <<"laborer"; break;
+    case 1: cout <<"secretary"; break;
+    case 2: cout <<"manager"; break;
+    case 3: cout <<"accountant"; break;
+    case 4: cout <<"executive"; break;
+    case 5: cout <<"researcher"; break;
+    }
+
+    cout << endl;
+
+    system ("pause");
+    return 0;
+}
