107b7082bf9f8814bf3555b6955700056eb277fc
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-22 15:49:55 +0300

    Update task5.cpp

--- Lab_3/task5.cpp
+++ Lab_3/task5.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+using namespace std;
+
+struct date
+{
+    int day;
+    int month;
+    int year;
+};
+
+int main()
+{
+    setlocale(0, "");
+    int dd, mm, yyyy;
+    char ch;
+
+    cout << "Введите день, месяц и год в формате dd/mm/yyyy." << endl;
+    cin >> dd >> ch >> mm >> ch >> yyyy;
+
+    date a = {dd, mm, yyyy};
+    cout <<a.day <<ch <<a.month <<ch <<a.year <<endl;
+
+    system("pause");
+    return 0;
+}
