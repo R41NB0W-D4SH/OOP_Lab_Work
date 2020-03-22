1993d4e24d571e95f51b4c5307b4f551b9e56067
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-22 15:28:50 +0300

    Update task4.cpp

--- Lab_3/task4.cpp
+++ Lab_3/task4.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
 
+using namespace std;
+
+struct employee
+{
+   int number;
+   float posobie;
+};
+
+int main()
+{
+    setlocale(0, "");
+    employee d1, d2, d3;
+
+    cout << "Введите номер первого сотрудника : ";  cin >> d1.number;
+    cout << "Введите пособие первого сотрудника : ";  cin >> d1.posobie;
+    cout << "Введите номер второго сотрудника : ";  cin >> d2.number;
+    cout << "Введите пособие второго сотрудника : ";  cin >> d2.posobie;
+    cout << "Введите номер третьего сотрудника : ";  cin >> d3.number;
+    cout << "Введите пособие третьего сотрудника : ";  cin >> d3.posobie;
+
+    cout << "У " << d1.number << "-го сотрудника пособие = " << d1.posobie << endl;
+    cout << "У " << d2.number << "-го сотрудника пособие = " << d2.posobie << endl;
+    cout << "У " << d3.number << "-го сотрудника пособие = " << d3.posobie << endl;
+
+
+    system("pause");
+    return 0;
+}
