e30db91f1264c8b992ae7dcb1e2be685458cb194
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-22 16:19:37 +0300

    Update task9.cpp

--- Lab_3/task9.cpp
+++ Lab_3/task9.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
 
+using namespace std;
+
+struct time
+{
+    int hour;
+    int min;
+    int sec;
+};
+
+int main()
+{
+    setlocale (0, "");
+    time t1;
+
+    cout <<"Введите часы, минуты и секунды: "; cin >>t1.hour >>t1.min >>t1.sec;
+
+    long totalsec =t1.hour*3600+t1.min*60+t1.sec;
+
+    cout <<"Количество секунд: " <<totalsec <<endl;
+
+    system ("pause");
+    return 0;
+}
