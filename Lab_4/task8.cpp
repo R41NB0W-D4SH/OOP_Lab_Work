23c6b5b0fefdb0fa36405c05c4e0519183c41188
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-27 20:15:38 +0300

    Update task8.cpp

--- Lab_4/task8.cpp
+++ Lab_4/task8.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+using namespace std;
+
+void swap(int&, int&);
+
+int main()
+{
+    setlocale(0, "");
+    int a, b;
+    cout <<"Введите первое число: "; cin >>a;
+    cout <<"Введите второе число: "; cin >>b;
+    swap(a, b);
+    cout <<"После обмена значений. Первое число: " <<a <<" второе: " <<b <<endl;
+    system("pause");
+    return 0;
+}
+void swap(int& first, int& second)
+{
+    int temp = first;
+    first = second;
+    second = temp;
+}
