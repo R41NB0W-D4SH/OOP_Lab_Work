c3da51013f67d8dbee9ce027c69b141b5b8768d5
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-27 20:37:11 +0300

    Update task10.cpp

--- Lab_4/task10.cpp
+++ Lab_4/task10.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+using namespace std;
+
+void func(int n)
+{
+    static int t;
+    cout <<"Функция вызывалась ранее " <<t <<" раз." <<endl;
+    t++;
+}
+
+int main()
+{
+    setlocale(0, "");
+    int n;
+    start:
+    cout <<"Введите количество вызовов функции > 10: ";
+    cin >>n;
+    if (n < 10) goto start;
+    else
+    for(int j = 0; j < n; j++) func(n);
+    system("pause");
+    return 0;
+}
