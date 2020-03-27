42215e693876dc5d101a8c8617eedff6951ada6a
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-27 18:54:03 +0300

    Update task1.cpp

--- Lab_4/task1.cpp
+++ Lab_4/task1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+float circarea(float radius);
+int main()
+{
+    setlocale(0, "");
+    double rad;
+    cout <<"Введите радиус окружности : ";
+    cin >>rad;
+    cout <<"Площадь равна : " <<circarea(rad)<<endl;
 
+    system("pause");
+    return 0;
+}
+float circarea(float r)
+{
+    const float PI = 3.14159F;
+    return r *r *PI;
+}
