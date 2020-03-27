65e455622c04529d3a4f06eec3da2a88213581c5
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-27 19:09:39 +0300

    Update task3.cpp

--- Lab_4/task3.cpp
+++ Lab_4/task3.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    void zeroSmaller(int&,int&);
+    int a = 4,b = 7,c = 11,d = 9;
+    zeroSmaller(a,b);
+    zeroSmaller(c,d);
+    cout <<"\na=" <<a <<" b==" <<b
+        <<" c==" <<c <<" d==" <<d;
 
+    system("pause");
+    return 0;
+}
+
+void zeroSmaller(int&first,int&second)
+{
+    if(first < second ) first = 0;
+    else second =0;
+}
