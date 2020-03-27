064c6fd75b40623038e27f347bddf7bf9663a0b4
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-03-27 20:01:18 +0300

    Update task5.cpp

--- Lab_4/task5.cpp
+++ Lab_4/task5.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <clocale>
 
+using namespace std;
+
+char ch;
+long hms_to_sec(int, int, int);
+
+int main()
+{
+    int h, m, s;
+    setlocale(0, "");
+    while(ch !='n')
+    {
+        cout <<"Введите время (ЧЧ:ММ:СС): "; cin >> h >> m >> s;
+        cout <<"Общее время в секундах: " << hms_to_sec(h, m, s) <<endl;
+        cout <<"Еще раз? (y/n): "; cin >> ch;
+    }
+    system("pause");
+    return 0;
+}
+long hms_to_sec(int hours, int min, int sec)
+{
+    return hours*3600+min*60+sec;
+}
