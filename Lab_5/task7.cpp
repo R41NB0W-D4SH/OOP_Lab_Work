8c52a77059afd0aa870842b170bb93805c86b164
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-04-02 01:18:51 +0300

    Update task7.cpp

--- Lab_5/task7.cpp
+++ Lab_5/task7.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
 
+class angle
+{
+   private:
+      int grad;
+      float min;
+      char direct;
+   public:
+      angle(): grad(0), min(0.0)
+      {}
+      angle(int g, float m, char d): grad(g), min(m), direct(d)
+      {}
+      void get_angle ()
+      {
+      cout<<"Введите координаты."<<endl;
+      cout<<"Градусы: ";
+      cin>>grad;
+      cout<<"Минуты: ";
+      cin>>min;
+      cout<<"Направление (N, S, E, W): ";
+      cin>>direct;
+      }
+      void show_angle()const
+      {cout << grad << '°' << min << "' " <<direct<<endl;}
+};
+
+int main()
+{
+setlocale (0, "");
+angle A1(100, 56.8, 'E');
+A1.show_angle();
+char n;
+do
+{
+    A1.get_angle();
+    A1.show_angle();
+    cout<<"\nЕще раз?(y/n): ";
+    cin>>n;
+}
+while (n !='n');
+system("pause");
+return 0;
+}
