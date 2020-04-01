10f4f456a5b7a003d8c2979ad4c38021384ccfe0
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-04-02 01:09:30 +0300

    Update task4.cpp

--- Lab_5/task4.cpp
+++ Lab_5/task4.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
 
+class employee
+{
+   private:
+       int nomer;
+       float posobie;
+   public:
+      employee(): nomer(0), posobie(0.0)
+      {}
+      void get_employee ()
+      {
+            cout<<"Введите номер: "; cin>>nomer;
+            cout<<"Введите оклад: "; cin>>posobie;
+      }
+      void show_employee()const
+      {cout<<"Номер сотрудника: "<<nomer<<" оклад: "<<posobie<<endl;}
+};
+
+int main()
+{
+setlocale (0, "");
+employee x1, x2, x3;
+x1.get_employee ();
+x2.get_employee ();
+x3.get_employee ();
+x1.show_employee();
+x2.show_employee();
+x3.show_employee();
+system("pause");
+return 0;
+}
