94be4be350f8d17ef427a72f42f60c941e93b254
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-04-02 01:11:01 +0300

    Update task5.cpp

--- Lab_5/task5.cpp
+++ Lab_5/task5.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+class date
+{
+   private:
+      int day, mons, year;
+   public:
+      date(): day(0), mons(0), year(0)
+      {}
+      void get_date ()
+      {
+      cout<<"Введите день, месяц и год в формате \"dd/mm/yy\":"<<endl;
+      cout<<"день ";
+      cin>>day;
+      cout<<"месяц: ";
+      cin>>mons;
+      cout<<"год: ";
+      cin>>year;
+      }
+      void show_date()const
+      {cout << day << "/" << mons << "/" <<year<<endl;}
+};
+
+int main()
+{
+setlocale (0, "");
+date x1, x2;
+x1.get_date();
+x2.get_date();
+x1.show_date();
+x2.show_date();
+system("pause");
+return 0;
+}
