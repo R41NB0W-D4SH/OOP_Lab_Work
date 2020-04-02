7ce8a6f40fad71fc63902fbf1551545d3bd44be7
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-04-03 00:07:34 +0300

    Update task8.cpp

--- Lab_5/task8.cpp
+++ Lab_5/task8.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+class podscet
+{
+   private:
+      int nomer;
+      static int vsego;
+   public:
+      podscet()
+        {vsego++; nomer = vsego;}
+      int getpodscet ()
+        {return  vsego;}
+      void show_podscet()const
+        {
+            cout << "Мой порядковый номер: " <<nomer<<endl;
+            cout << "Всего объектов: " <<vsego<<endl;
+        }
+};
+
+int podscet::vsego = 0;
+
+int main()
+{
+setlocale (0 "");
+podscet p1, p2, p3;
+p1.show_podscet();
+p2.show_podscet();
+p3.show_podscet();
+system("pause");
+return 0;
+}
