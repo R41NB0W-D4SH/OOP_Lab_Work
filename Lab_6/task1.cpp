3c5c5f7e4441958e84f5eb5607e46d94fdc1b426
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-04-03 19:24:10 +0300

    Update task1.cpp

--- Lab_6/task1.cpp
+++ Lab_6/task1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    void reversit(char [] );  //прототип
+    const int MAX = 80;         //размер массива
+    char str [MAX];            //строка
+    cout <<"Введите строку:";//получить строку от пользователя
+    cin.get(str,MAX);
+    reversit(str);             //перевернуть строку
+    cout <<"Перевернутая строка : ";//и вывести ее
+    cout <<str <<endl;
+    system("pause");
+    return 0;
+}
 
+void reversit(char s[])
+{
+    int len = strlen(s);        //найти длину строки
+    for(int j = 0;j < len / 2; j++) //поменять все символы из первой половины
+    {
+        char temp = s[j];        //на символы
+        s[j] = s [len - j - 1];     //из второй половины
+        s[len - j - 1] = temp;
+    }
+}
