8bbb83087382c7784d3fde1291563d236104e38e
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-04-03 19:31:02 +0300

    Update task2.cpp

--- Lab_6/task2.cpp
+++ Lab_6/task2.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class employee
+    {
+    private:
+      string name;
+      long number;
+    public:
+      void getdata()    //получить данные от пользователя
+        {
+        cout <<"\nВведите имя:";cin >>name;
+        cout <<"Введите номер:";cin >>number;
+        }
+      void putdata()    //вывод данных
+        {
+        cout <<"\nИмя:" <<name;
+        cout <<"\nНомер:" <<number;
+        }
+    };
+
+int main()
+    {
+    setlocale(0, "");
+    employee emparr [100]; //массив типа employee
+    int n =0;              //количество работников
+    char ch;               //ответ пользователя
+    do
+    {                   //получить данные от пользователя
+      cout <<"Введите данные о работнике с номером "             <<n+1;
+      emparr [n++].getdata();
+      cout <<"Продолжить (y/n)?";cin >>ch;
+    }
+     while(ch != 'n' || ch != 'N');
+    for(int j=0;j<n;j++)   //вывести данные из массива
+    {
+      cout <<"\nНомер работника " <<j+1;
+      emparr [j].putdata();
+    }
+    cout <<endl;
+    system("pause");
+    return 0;
+    }
