fc9d9b60b57baec408a8180c4a80bcd285cb6903
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-04-05 22:37:32 +0300

    Update task7.cpp

--- Lab_6/task7.cpp
+++ Lab_6/task7.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#include <cstring>
 
+using namespace std;
+
+class money
+{
+private:
+    long double cash;
+    char in[80];
+    char out[80];
+public:
+    money(){cash=0.0;}
+    money(long double d):cash(d){}
+    void input()
+    {
+        cout <<"Введите сумму: "; cin >>in;
+    }
+    void outg()
+    {
+        cout <<setiosflags(ios::fixed) <<setprecision(2) <<mstold(in) <<endl;
+    }
+    long double mstold(char mass[])
+    {
+        int n=0;
+        for(int j=0; j<strlen(mass); j++)
+        {
+            if((mass[j]=='$')||(mass[j]==','))
+                continue;
+            out[n++]=mass[j];
+        }
+        double cash=atof(out);//возвращение дабл хоть както
+        return cash;
+    }
+};
+
+int main()
+{
+    setlocale(0,"");
+    int n=0;
+    char ch;
+    money m[100];
+    do{
+        m[n++].input();
+        cout <<"Еще раз (y/n)?";
+        cin >>ch;
+    }while(ch != 'n');
+    for(int j=0; j<n; j++)
+        m[j].outg();
+    system("pause");
+}
