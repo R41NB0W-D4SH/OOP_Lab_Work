2cb2ff29fbb545f2e1ff0bcb814a54e9911d280a
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-04-05 23:04:46 +0300

    Update task9.cpp

--- Lab7/task9.cpp
+++ Lab7/task9.cpp
@@ -1,1 +1,48 @@
-
+const int LIMIT=100;
+//////////////////////////////////////////////////////////////////
+class safearray
+{
+private:
+    int min;
+    int max;
+    int arr[LIMIT];
+public:
+    safearray()
+    {min=0, max=0;}
+    safearray(int mn, int mx):min(mn), max(mx)
+    {}
+    void getin(){
+        cout <<"Введите min элемент массива: ";
+        cin >>min;
+        cout <<"Введите max элемент массива: ";
+        cin >>max;
+        int puk=max-min;
+        if(puk>100)exit(1);
+        //задаем значения элементов      
+        for(int j=0; j<=puk; j++)
+        arr[j]=j*10;//используем ф-ию слева от знака
+        //показываем элементы
+        for(int j=0; j<=puk; j++){
+        int temp=arr[j];//используем ф-ию справа от знака
+        cout <<"Элемент " <<j+min <<" равен " <<temp <<endl;
+        }
+    }
+    //функция возвращает ссылку
+    int& operator[](int n)
+    {
+        if(n<min||n>=max){
+            cout <<"Ошибочный индекс!";
+            exit(1);
+        }
+        return arr[n];
+    }
+};
+/////////////////////////////////////////////////////////////////
+int _tmain(int argc, _TCHAR* argv[])
+{
+    setlocale(LC_ALL,"");
+    safearray sa;
+    sa.getin();
+    system("pause");
+    return 0;
+}
