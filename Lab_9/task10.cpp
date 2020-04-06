480315e9b74871ab06261c3265bddfeb83348f22
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-04-06 19:38:10 +0300

    Update task10.cpp

--- Lab_9/task10.cpp
+++ Lab_9/task10.cpp
@@ -1,1 +1,17 @@
-
+#include <iostream>
+using namespace std;
+int main ( )
+{
+    const int n=10;
+    int* ap[n];
+    for(int j=0;j<n;j++)
+        *(ap+j)=new int[n];
+    for(int j=0;j<10;j++)
+        for(int k=0;k<10;k++)
+            *(*(ap+j)+k)=k*10;
+    for(int j=0;j<10;j++)
+        for(int k=0;k<10;k++)
+            cout<<*(*(ap+j)+k)<<endl;
+system("pause");
+  return 0;
+}
