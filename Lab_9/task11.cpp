6f5767b9e1ba63f5868263635fd852b3b26ca9f8
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-04-06 19:38:59 +0300

    Update task11.cpp

--- Lab_9/task11.cpp
+++ Lab_9/task11.cpp
@@ -1,1 +1,45 @@
-
+#include <iostream>
+using namespace std;
+ 
+class Massiv
+{
+private:
+ 
+    int* ap;
+    int n;
+    int m;
+public:
+    Massiv(int n_=100, int m_=10): n(n_),m(m_)
+    {
+    ap=new int[n];
+    setValue();
+    }
+    void setValue()
+    {
+    for(int k=0;k<n;)
+     for(int i=0;i<m;i++)
+        *(ap+k++)=i*m;
+    }
+ 
+    int& operator[] (int i)
+    {
+        return ap[i];
+    }
+    void show(){
+for(int i=0;i<n;i++) 
+cout<<*(ap+i)<<endl;
+    cout<<endl;
+}
+ 
+ 
+ 
+};
+ 
+int main ( )
+{
+ 
+    Massiv m;
+    m.show();
+system("pause");
+  return 0;
+}
