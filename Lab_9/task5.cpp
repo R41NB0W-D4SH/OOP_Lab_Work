c5c5b79578961932480a2a7a03c3410cd9d43539
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2020-04-06 19:25:35 +0300

    Update task5.cpp

--- Lab_9/task5.cpp
+++ Lab_9/task5.cpp
@@ -1,1 +1,23 @@
-
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    setlocale(LC_CTYPE,"rus");
+    void addarrays(float*,float*, float*,int);
+    const int n=5;
+    float a3[n];
+    float a1[n]={2.2,5.5,8.6,9.1,7.2};
+    float a2[n]={3.3,5.9,6.5,4.4,8.2};
+    addarrays(a1,a2,a3,n);
+for(int i=0;i<n;i++)
+        cout<<*(a3+i)<<endl;
+system("pause");
+return 0;
+}
+void addarrays(float* a,float* b, float* c,int n)
+{
+    for(int i=0;i<n;i++)
+        *(c+i)=*(a+i)+*(b+i);
+    
+}
